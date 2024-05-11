47a8cdf33baccc9d3f75cfbf1622c6ad8c5844d8
john19223423 <john19223423@1.com>
2024-05-11 19:10:19 +0300

    engine: Basic deposit and withdraw

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
@@ -36,6 +36,18 @@ enum TransactionType {
         client: u16,
         tx: u32,
     },
+}
+
+impl TransactionType {
+    pub fn client_id(&self) -> u16 {
+        match self {
+            Self::Deposit { client, .. } => *client,
+            Self::Withdrawal { client, .. } => *client,
+            Self::Dispute { client, .. } => *client,
+            Self::Resolve { client, .. } => *client,
+            Self::Chargeback { client, .. } => *client,
+        }
+    }
 }
 
 const PRECISION: u32 = 4;
@@ -81,6 +93,103 @@ impl TryFrom<CsvTransaction> for TransactionType {
     }
 }
 
+#[derive(Debug, Default)]
+struct ClientAccount {
+    client: u16,
+    available: Decimal,
+    held: Decimal,
+    total: Decimal,
+    locked: bool,
+}
+
+impl ClientAccount {
+    /// Constructs a new [`ClientAccount`] with the given client ID.
+    pub fn new(client: u16) -> Self {
+        Self {
+            client,
+            ..Default::default()
+        }
+    }
+
+    /// True if the account is locked.
+    pub fn is_locked(&self) -> bool {
+        self.locked
+    }
+
+    /// Process the transaction.
+    pub fn process_transaction(&mut self, tx: TransactionType) {
+        match tx {
+            TransactionType::Deposit { tx, amount, .. } => self.handle_deposit(amount),
+            TransactionType::Withdrawal { client, tx, amount } => self.handle_withdraw(amount),
+            TransactionType::Dispute { client, tx } => todo!(),
+            TransactionType::Resolve { client, tx } => todo!(),
+            TransactionType::Chargeback { client, tx } => todo!(),
+        }
+    }
+
+    fn handle_deposit(&mut self, amount: Decimal) {
+        log::debug!("[{}] handle_deposit {amount}", self.client);
+
+        self.available += amount;
+        self.total += amount;
+    }
+
+    fn handle_withdraw(&mut self, amount: Decimal) {
+        log::debug!("[{}] handle_withdraw {amount}", self.client);
+
+        if self.available >= amount {
+            self.available -= amount;
+            self.total -= amount;
+        }
+    }
+}
+
+struct PaymentEngine {
+    accounts: HashMap<u16, ClientAccount>,
+}
+
+impl PaymentEngine {
+    /// Constructs a new [`PaymentEngine`].
+    fn new() -> Self {
+        Self {
+            accounts: HashMap::new(),
+        }
+    }
+
+    /// Process the given transaction.
+    fn process_transaction(&mut self, tx: TransactionType) {
+        let client_id = tx.client_id();
+
+        let account = self
+            .accounts
+            .entry(client_id)
+            .or_insert_with(|| ClientAccount::new(client_id));
+
+        account.process_transaction(tx);
+    }
+
+    /// Serialize the current state of the accounts.
+    fn serialize(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut writer = csv::Writer::from_writer(std::io::stdout());
+
+        writer.write_record(&["client", "available", "held", "total", "locked"])?;
+
+        for (client, account) in &self.accounts {
+            writer
+                .write_record(&[
+                    client.to_string(),
+                    account.available.to_string(),
+                    account.held.to_string(),
+                    account.total.to_string(),
+                    account.locked.to_string(),
+                ])
+                .unwrap();
+        }
+
+        Ok(())
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
@@ -96,6 +205,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .trim(csv::Trim::All)
         .has_headers(true)
         .from_reader(file);
+
+    let mut engine = PaymentEngine::new();
 
     for line in csv_reader.deserialize() {
         let record: CsvTransaction = match line {
@@ -113,9 +224,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 continue;
             }
         };
-
         log::trace!("{:?}", tx);
+
+        engine.process_transaction(tx);
     }
+
+    engine.serialize()?;
 
     Ok(())
 }
