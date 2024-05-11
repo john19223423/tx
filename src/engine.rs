4639a9d8e466ba715caae61353bf6afba2e20069
john19223423 <john19223423@1.com>
2024-05-11 20:37:51 +0300

    engine/tests: Check multi accounts

--- src/engine.rs
+++ src/engine.rs
@@ -49,3 +49,95 @@ impl PaymentEngine {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::TransactionType;
+    use rust_decimal::Decimal;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_multiaccount_deposit() {
+        let mut engine = PaymentEngine::new();
+
+        let tx = TransactionType::Deposit {
+            client: 1,
+            tx: 1,
+            amount: Decimal::from_str("1.0").unwrap(),
+        };
+
+        engine.process_transaction(tx);
+
+        let account = engine.accounts.get(&1).unwrap();
+        assert_eq!(account.available, Decimal::from_str("1.0").unwrap());
+        assert_eq!(account.held, Decimal::from_str("0.0").unwrap());
+        assert_eq!(account.total, Decimal::from_str("1.0").unwrap());
+        assert_eq!(account.locked, false);
+
+        let tx = TransactionType::Deposit {
+            client: 2,
+            tx: 2,
+            amount: Decimal::from_str("4.0").unwrap(),
+        };
+
+        engine.process_transaction(tx);
+
+        // Account 1 unaffected.
+        let account = engine.accounts.get(&1).unwrap();
+        assert_eq!(account.available, Decimal::from_str("1.0").unwrap());
+        assert_eq!(account.held, Decimal::from_str("0.0").unwrap());
+        assert_eq!(account.total, Decimal::from_str("1.0").unwrap());
+        assert_eq!(account.locked, false);
+
+        // Account 2 updated.
+        let account = engine.accounts.get(&2).unwrap();
+        assert_eq!(account.available, Decimal::from_str("4.0").unwrap());
+        assert_eq!(account.held, Decimal::from_str("0.0").unwrap());
+        assert_eq!(account.total, Decimal::from_str("4.0").unwrap());
+        assert_eq!(account.locked, false);
+    }
+
+    #[test]
+    fn test_multiaccount_withdraw() {
+        let mut engine = PaymentEngine::new();
+
+        let tx = TransactionType::Deposit {
+            client: 1,
+            tx: 1,
+            amount: Decimal::from_str("1.0").unwrap(),
+        };
+        engine.process_transaction(tx);
+        let tx = TransactionType::Deposit {
+            client: 2,
+            tx: 2,
+            amount: Decimal::from_str("4.0").unwrap(),
+        };
+        engine.process_transaction(tx);
+
+        let tx = TransactionType::Withdrawal {
+            client: 1,
+            tx: 3,
+            amount: Decimal::from_str("0.5").unwrap(),
+        };
+        engine.process_transaction(tx);
+        let tx = TransactionType::Withdrawal {
+            client: 2,
+            tx: 4,
+            amount: Decimal::from_str("1.0").unwrap(),
+        };
+        engine.process_transaction(tx);
+
+        let account = engine.accounts.get(&1).unwrap();
+        assert_eq!(account.available, Decimal::from_str("0.5").unwrap());
+        assert_eq!(account.held, Decimal::from_str("0.0").unwrap());
+        assert_eq!(account.total, Decimal::from_str("0.5").unwrap());
+        assert_eq!(account.locked, false);
+
+        let account = engine.accounts.get(&2).unwrap();
+        assert_eq!(account.available, Decimal::from_str("3.0").unwrap());
+        assert_eq!(account.held, Decimal::from_str("0.0").unwrap());
+        assert_eq!(account.total, Decimal::from_str("3.0").unwrap());
+        assert_eq!(account.locked, false);
+    }
+}
