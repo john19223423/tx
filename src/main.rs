c69fddb27ba8fb4057ef5b9a59ea13012f056dd8
john19223423 <john19223423@1.com>
2024-05-11 19:42:30 +0300

    engine: Handle dispute, resolves and chargebacks
    Signed-off-by: john19223423 <john19223423@1.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
@@ -12,7 +15,7 @@ struct CsvTransaction {
     amount: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 enum TransactionType {
     Deposit {
         client: u16,
@@ -39,6 +42,7 @@ enum TransactionType {
 }
 
 impl TransactionType {
+    /// Returns the client ID associated with the transaction.
     pub fn client_id(&self) -> u16 {
         match self {
             Self::Deposit { client, .. } => *client,
@@ -46,6 +50,17 @@ impl TransactionType {
             Self::Dispute { client, .. } => *client,
             Self::Resolve { client, .. } => *client,
             Self::Chargeback { client, .. } => *client,
+        }
+    }
+
+    /// Returns the transaction ID associated with the transaction.
+    pub fn transaction_id(&self) -> u32 {
+        match self {
+            Self::Deposit { tx, .. } => *tx,
+            Self::Withdrawal { tx, .. } => *tx,
+            Self::Dispute { tx, .. } => *tx,
+            Self::Resolve { tx, .. } => *tx,
+            Self::Chargeback { tx, .. } => *tx,
         }
     }
 }
@@ -93,6 +108,14 @@ impl TryFrom<CsvTransaction> for TransactionType {
     }
 }
 
+#[derive(Debug)]
+enum ClientErr {
+    AccountLocked,
+    InsufficientFunds,
+    DisputedTransactionNotFound,
+    AlreadyProcessed,
+}
+
 #[derive(Debug, Default)]
 struct ClientAccount {
     client: u16,
@@ -100,6 +123,9 @@ struct ClientAccount {
     held: Decimal,
     total: Decimal,
     locked: bool,
+
+    processed_tx: HashMap<u32, TransactionType>,
+    under_dispute: HashSet<u32>,
 }
 
 impl ClientAccount {
@@ -117,30 +143,107 @@ impl ClientAccount {
     }
 
     /// Process the transaction.
-    pub fn process_transaction(&mut self, tx: TransactionType) {
-        match tx {
-            TransactionType::Deposit { tx, amount, .. } => self.handle_deposit(amount),
-            TransactionType::Withdrawal { client, tx, amount } => self.handle_withdraw(amount),
-            TransactionType::Dispute { client, tx } => todo!(),
-            TransactionType::Resolve { client, tx } => todo!(),
-            TransactionType::Chargeback { client, tx } => todo!(),
+    pub fn process_transaction(&mut self, tx: TransactionType) -> Result<(), ClientErr> {
+        if self.is_locked() {
+            return Err(ClientErr::AccountLocked);
         }
+
+        let tx_id = tx.transaction_id();
+        if self.processed_tx.contains_key(&tx_id) {
+            return Err(ClientErr::AlreadyProcessed);
+        }
+
+        match tx {
+            TransactionType::Deposit { amount, .. } => self.handle_deposit(amount)?,
+            TransactionType::Withdrawal { amount, .. } => self.handle_withdraw(amount)?,
+            TransactionType::Dispute { tx, .. } => self.handle_dispute(tx)?,
+            TransactionType::Resolve { tx, .. } => self.handle_resolve(tx)?,
+            TransactionType::Chargeback { tx, .. } => self.handle_chargeback(tx)?,
+        }
+
+        self.processed_tx.insert(tx_id, tx);
+        Ok(())
     }
 
-    fn handle_deposit(&mut self, amount: Decimal) {
-        log::debug!("[{}] handle_deposit {amount}", self.client);
+    fn handle_deposit(&mut self, amount: Decimal) -> Result<(), ClientErr> {
+        log::debug!("[client {}] handle_deposit {amount} ", self.client);
 
         self.available += amount;
         self.total += amount;
+
+        Ok(())
     }
 
-    fn handle_withdraw(&mut self, amount: Decimal) {
-        log::debug!("[{}] handle_withdraw {amount}", self.client);
-
-        if self.available >= amount {
-            self.available -= amount;
-            self.total -= amount;
+    fn handle_withdraw(&mut self, amount: Decimal) -> Result<(), ClientErr> {
+        log::debug!("[client {}] handle_withdraw {amount}", self.client);
+        if self.available < amount {
+            return Err(ClientErr::InsufficientFunds);
         }
+
+        self.available -= amount;
+        self.total -= amount;
+        Ok(())
+    }
+
+    fn handle_dispute(&mut self, tx: u32) -> Result<(), ClientErr> {
+        log::debug!("[client {}] handle_dispute {tx}", self.client);
+
+        let tx = self
+            .processed_tx
+            .get(&tx)
+            .ok_or(ClientErr::DisputedTransactionNotFound)?;
+
+        if let TransactionType::Deposit { amount, .. } = tx {
+            self.available -= amount;
+            self.held += amount;
+
+            self.under_dispute.insert(tx.transaction_id());
+        }
+
+        Ok(())
+    }
+
+    fn handle_resolve(&mut self, tx: u32) -> Result<(), ClientErr> {
+        log::debug!("[client {}] handle_resolve {tx}", self.client);
+
+        let disputed_tx = self
+            .processed_tx
+            .get(&tx)
+            .ok_or(ClientErr::DisputedTransactionNotFound)?;
+
+        // Tx must be marked as disputed to resolve it.
+        if !self.under_dispute.remove(&disputed_tx.transaction_id()) {
+            return Err(ClientErr::DisputedTransactionNotFound);
+        }
+
+        if let TransactionType::Deposit { amount, .. } = disputed_tx {
+            self.available += amount;
+            self.held -= amount;
+        }
+
+        Ok(())
+    }
+
+    fn handle_chargeback(&mut self, tx: u32) -> Result<(), ClientErr> {
+        log::debug!("[client {}] handle_chargeback {tx}", self.client);
+
+        let disputed_tx = self
+            .processed_tx
+            .get(&tx)
+            .ok_or(ClientErr::DisputedTransactionNotFound)?;
+
+        // Tx must be marked as disputed to chargeback it.
+        if !self.under_dispute.remove(&disputed_tx.transaction_id()) {
+            return Err(ClientErr::DisputedTransactionNotFound);
+        }
+
+        if let TransactionType::Deposit { amount, .. } = disputed_tx {
+            self.held -= amount;
+            self.total -= amount;
+            self.locked = true;
+        }
+
+        Ok(())
     }
 }
 
@@ -165,14 +268,16 @@ impl PaymentEngine {
             .entry(client_id)
             .or_insert_with(|| ClientAccount::new(client_id));
 
-        account.process_transaction(tx);
+        if let Err(err) = account.process_transaction(tx) {
+            log::error!("[{}] Error processing transaction: {:?}", client_id, err);
+        }
     }
 
     /// Serialize the current state of the accounts.
     fn serialize(&self) -> Result<(), Box<dyn std::error::Error>> {
         let mut writer = csv::Writer::from_writer(std::io::stdout());
 
-        writer.write_record(&["client", "available", "held", "total", "locked"])?;
+        writer.write_record(["client", "available", "held", "total", "locked"])?;
 
         for (client, account) in &self.accounts {
             writer
