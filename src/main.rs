d922271001c9c5ff33d389dca5dac76d34a21848
john19223423 <john19223423@1.com>
2024-05-11 18:18:33 +0300

    csv: Read and decode transaction
    Signed-off-by: john19223423 <john19223423@1.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CsvTransaction {
+    #[serde(rename = "type")]
+    ty: String,
+    client: u16,
+    tx: u32,
+    amount: f64,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let Some(file_location) = std::env::args().nth(1) else {
+        return Err("Please provide a file location".into());
+    };
+    log::debug!("File location: {}", file_location);
+
+    let file = std::fs::File::open(file_location)?;
+
+    let mut csv_reader = csv::ReaderBuilder::new()
+        .flexible(true)
+        .trim(csv::Trim::All)
+        .has_headers(true)
+        .from_reader(file);
+
+    for line in csv_reader.deserialize() {
+        let Ok(record) = line.map_err(|err| {
+            log::error!("Unprocessed line {err:?}");
+            err
+        }) else {
+            continue;
+        };
+        let record: CsvTransaction = record;
+
+        log::trace!("{:?}", record);
+    }
+
+    Ok(())
 }
